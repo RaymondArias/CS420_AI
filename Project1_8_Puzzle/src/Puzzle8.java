942a7c30610d32f2d1e85e0f1a3ba9e0441883b9
Raymond B Arias <Raymond.B.Arias@boeing.com>
2016-10-13 11:09:40 -0700

    Fixed isSolvable bug and added user input option

--- Project1_8_Puzzle/src/Puzzle8.java
+++ Project1_8_Puzzle/src/Puzzle8.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Puzzle8 {
-	public Stack<PuzzleBoard> aStarSearch(int [][] puzzle)
+	public Stack<PuzzleBoard> aStarSearch(int [][] puzzle, int heuristicFunction)
 	{
 		//Create initial state of problem
-		PuzzleBoard node = new PuzzleBoard(puzzle, null, 0, 0);
+		PuzzleBoard node = new PuzzleBoard(puzzle, null, 0, heuristicFunction);
+		if(!node.isSolvable())
+		{
+			System.out.println("Puzzle not solvable");
+			return new Stack<>();
+		}
 		//Create frontier and add initial state
 		PriorityQueue<PuzzleBoard> frontier 
 		= new PriorityQueue<PuzzleBoard>(new PuzzleComparator());
@@ -22,6 +29,16 @@ public class Puzzle8 {
 			leaf = frontier.poll();
 			if(leaf.isGoalState())
 			{
+				if(heuristicFunction == 0)
+				{
+					System.out.println("Heuristic Function: Misplaced Tiles");
+				}
+				else
+				{
+					System.out.println("Heuristic Function: Manhattan Distance");
+				}
+				System.out.println("Depth: " + leaf.getPathCost());
+				System.out.println("Nodes Generated: " + (frontier.size() + alreadyVisited.size()));
 				break;
 			}
 			//Add it alreadyVisted table
@@ -32,7 +49,7 @@ public class Puzzle8 {
 			for(int i = 0; i < possibleMoves.size(); i++)
 			{
 				int [][]puzzleMove = possibleMoves.get(i);
-				puzzleConfig = new PuzzleBoard(puzzleMove, leaf, leaf.getPathCost() + 1, 0);
+				puzzleConfig = new PuzzleBoard(puzzleMove, leaf, leaf.getPathCost() + 1, heuristicFunction);
 				if(!alreadyVisited.contains(puzzleConfig.hash()))
 				{
 					frontier.add(puzzleConfig);
@@ -116,24 +133,87 @@ public class Puzzle8 {
 		    child[i] = Arrays.copyOf(parent[i],parent.length);
 		return child;
 	}
-	public static void main(String []args)
+	public int [][] generateRandomPuzzles(int [][]puzzle, int shuffles)
 	{
-		int [][] puzzle = {{0,1, 2}, {4, 5, 6}, {3, 8, 7}};
-		Puzzle8 p8 =  new Puzzle8();
-		Stack<PuzzleBoard> moves = p8.aStarSearch(puzzle);
-		ArrayList<int[][]> puzzles = p8.possiblePuzzleMoves(puzzle);
+	
+		ArrayList<int [][]> puzzleBoards = null;
+		Random random = new Random();
+		for(int i = 0; i < shuffles; i++)
+		{
+			puzzleBoards = this.possiblePuzzleMoves(puzzle);
+			puzzle = puzzleBoards.get(random.nextInt(puzzleBoards.size()));
+		}
+		PuzzleBoard pb = new PuzzleBoard(puzzle, null, 0, 0);
+		if(!pb.isSolvable())
+			return generateRandomPuzzles(puzzle, 100);
+		return puzzle;
+	}
+	public void printMoves(Stack<PuzzleBoard>moves)
+	{
+		int counter = 0;
 		while(!moves.isEmpty())
 		{
 			int [][]puzzleConfig = moves.pop().getPuzzle();
+			System.out.println("Move: " + counter);
 			for(int i = 0; i < puzzleConfig.length; i++)
 			{
 				for (int j = 0; j < puzzleConfig[i].length; j++)
 				{
 					System.out.print(puzzleConfig[i][j]);
 				}
+				
 				System.out.println();
 			}
+			counter++;
 			System.out.println();
+			
+		}
+	}
+	public static void main(String []args)
+	{
+		/*int [][] puzzle = {{0, 1, 2}, {3,4,5}, {6,7,8}};
+		Puzzle8 p8 =  new Puzzle8();
+		puzzle = p8.generateRandomPuzzles(puzzle, 100);
+		Stack<PuzzleBoard> moves = p8.aStarSearch(puzzle, 1);
+		p8.printMoves(moves);*/
+		Scanner scanner = new Scanner(System.in);
+		Puzzle8 puzzle8 = new Puzzle8();
+		while(true)
+		{
+			System.out.println("Please choose a command: ");
+			System.out.println("1.Enter a puzzle");
+			System.out.println("2. 100 Random puzzles");
+			int userChoice = scanner.nextInt();
+			if(userChoice == 1)
+			{
+				scanner.nextLine();
+				System.out.println("Please Enter each Row");
+				String line1 = scanner.nextLine();
+				String line2 = scanner.nextLine();
+				String line3 = scanner.nextLine();
+				String []lineOneData = line1.split(" ");
+				String []lineTwoData = line2.split(" ");
+				String []lineThreeData = line3.split(" ");
+				int [][]userPuzzle = {{Integer.parseInt(lineOneData[0]), Integer.parseInt(lineOneData[1]), Integer.parseInt(lineOneData[2])},
+				{Integer.parseInt(lineTwoData[0]), Integer.parseInt(lineTwoData[1]), Integer.parseInt(lineTwoData[2])},
+				{Integer.parseInt(lineThreeData[0]), Integer.parseInt(lineThreeData[1]), Integer.parseInt(lineThreeData[2])}};
+				Stack<PuzzleBoard> moves = puzzle8.aStarSearch(userPuzzle, 0);
+				puzzle8.printMoves(moves);
+			}
+			else if(userChoice == 2)
+			{
+				for (int i = 0; i < 200; i++)
+				{
+					System.out.println("Puzzle: " + (i + 1));
+					int [][] puzzle = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+					puzzle = puzzle8.generateRandomPuzzles(puzzle, 100);
+					Stack<PuzzleBoard> moves = puzzle8.aStarSearch(puzzle, 0);
+					Stack<PuzzleBoard> moves1 = puzzle8.aStarSearch(puzzle, 1);
+					System.out.println();
+					
+					//puzzle8.printMoves(moves);
+				}
+			}
 			
 		}
 	}
--- Project1_8_Puzzle/src/PuzzleBoard.java
+++ Project1_8_Puzzle/src/PuzzleBoard.java
@@ -20,6 +20,16 @@ public class PuzzleBoard {
 				this.goalState = true;
 			this.aStar = pathCost + misplacedTiles();
 		}
+		else if(heuristicType == 1)
+		{
+			//Do manhattan distance heuristic function
+			int manhattanDistance = manhattanDistance();
+			if(manhattanDistance == 0)
+			{
+				this.goalState = true;
+			}
+			this.aStar = pathCost + manhattanDistance;
+		}
 	}
 	public PuzzleBoard()
 	{
@@ -84,6 +94,26 @@ public class PuzzleBoard {
 		}
 		return misplacedTiles;
 	}
+	public int manhattanDistance()
+	{
+		int manhattanDistance = 0;
+		for (int i = 0; i < puzzle.length; i++)
+		{
+			for(int j = 0; j < puzzle[i].length; j++)
+			{
+				int value = puzzle[i][j];
+				if(value != 0)
+				{
+					int expectedX = (value) / puzzle.length;
+					int expectedY = (value) % puzzle.length;
+					int dx = i - expectedX;
+					int dy = j - expectedY;
+					manhattanDistance += Math.abs(dx) + Math.abs(dy);
+				}
+			}
+		}
+		return manhattanDistance;
+	}
 	public int hash()
 	{
 		int hashValue = 0;
@@ -98,14 +128,53 @@ public class PuzzleBoard {
 		}
 		return hashValue;
 	}
+	public boolean isSolvable()
+	{
+		//Keep track of invertedTiles
+		int []arrayTransform = new int [9];
+		int index = 0;
+		for(int i = 0; i < puzzle.length; i++)
+		{
+			for(int j = 0; j < puzzle[i].length; j++)
+			{
+				arrayTransform[index] = puzzle[i][j];
+				
+			}
+		}
+		//if invertedTiles is not even then unsolvable
+		return (findInvertedTiles(arrayTransform) % 2) == 0;
+	}
+	public int findInvertedTiles(int []array)
+	{
+		int invertedTiles = 0;
+		for (int i = 0; i < array.length; i++)
+		{
+			int value = array[i];
+			if (value != 0)
+			{
+				for(int j = i; j < array.length; j++)
+				{
+					if(value > array[j] && array[j] != 0)
+					{
+						invertedTiles++;
+					}
+					
+				}
+			}
+		}
+		return invertedTiles;
+	}
 	public static void main(String []args)
 	{
 		int [][]puzzleConfig = {{0,1,2}, {3,4,5}, {6,7,8}};
-		int [][]puzzleConfig1 = {{1,0,2}, {3,4,5}, {6,7,8}};
+		int [][]puzzleConfig1 = {{1,0,2}, {6,4,5}, {3,7,8}};
 		int [][]puzzleConfig2 = {{8,0,8}, {8,0,8}, {8,0,8}};
 		PuzzleBoard pb = new PuzzleBoard(puzzleConfig, null, 0, 0);
 		System.out.println(pb.hash());
+		System.out.println(pb.manhattanDistance());
+		
 		pb.setPuzzle(puzzleConfig1);
+		System.out.println(pb.isSolvable());
 		System.out.println(pb.hash());
 		pb.setPuzzle(puzzleConfig2);
 		System.out.println(pb.hash());
